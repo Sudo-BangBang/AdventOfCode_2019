a1d5ded8d1991cd28067bc7fd0283217401e3405
Sudo-BangBang <s.j.tay@outlook.com>
2019-12-10 16:13:39 +0000

    Day 4

---
+++ Day4/Part1.js
@@ -0,0 +1,27 @@
+let min = 168630;
+let max = 718098;
+let validCodeCount = 0;
+
+let i = min;
+
+function hasAdjacentDuplicate(code){
+	return code.toString().split("").filter((digit, index) => { return index<code.toString().length-1 && digit === code.toString()[index+1]}).length > 0;
+}
+
+function digitsDontDecrease(code){
+	return code.toString().split("").filter((digit, index) => { return index<code.toString().length-1 && digit <= code.toString()[index+1]}).length === code.toString().length-1;
+}
+
+function codeIsValid(code) {
+	return digitsDontDecrease(code) && hasAdjacentDuplicate(code);	
+}
+
+while (i <= max){
+	if(codeIsValid(i)){
+		validCodeCount++;
+	}
+
+	i++;
+}
+
+document.querySelector("#app").innerHTML = validCodeCount;
